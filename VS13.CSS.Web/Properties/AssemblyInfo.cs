444ae9e5a97b4cb8b134f747a067db42299c98fa
jpheary <james.heary@hotmail.com>
2015-03-13 13:49:20 -0400

    Investigated fixed/fluid/adaptive/responsive designs

--- VS13.CSS.Web/Properties/AssemblyInfo.cs
+++ VS13.CSS.Web/Properties/AssemblyInfo.cs
@@ -2,34 +2,17 @@
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
 [assembly: AssemblyTitle("VS13.CSS.Web")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("VS13.CSS.Web")]
-[assembly: AssemblyCopyright("Copyright ©  2014")]
+[assembly: AssemblyDescription("Exploring CSS")]
+[assembly: AssemblyConfiguration("Updated 03/06/15, jph")]
+[assembly: AssemblyCompany("jpHeary")]
+[assembly: AssemblyProduct("CSS")]
+[assembly: AssemblyCopyright("Copyright jpHeary 2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
-
-// The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("29054e91-9fc0-452d-9bdc-1917c7cda066")]
 
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Revision and Build Numbers 
-// by using the '*' as shown below:
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("4.5.0.0")]
+[assembly: AssemblyFileVersion("4.5.0.0")]
