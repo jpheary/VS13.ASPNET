0c2c0a398bea1693e6fa44987d5461831c59c309
jpheary <james.heary@hotmail.com>
2015-11-10 14:12:46 -0500

    Added some common jQuery patterns

--- VS13.jQuery.Mvc/Controllers/jQueryController.cs
+++ VS13.jQuery.Mvc/Controllers/jQueryController.cs
@@ -1,29 +1,38 @@
-﻿using System.Web.Mvc;
-using VS13.Models;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
 
 namespace VS13.Controllers {
     public class jQueryController:Controller {
-        // GET: jQuery
-        public ActionResult Index() { return View(); }
-        public ActionResult Calendar() { return View(new FromToDates()); }        
-        public ActionResult Spinner() { return View(); }
-        public ActionResult TabsH() { return View(); }
-        public ActionResult TabsV() { return View(); }
-        public ActionResult TimeSpinner() { return View(); }
+        //Members
 
 
-        public ActionResult Shipment() { return View(new Shipment()); }
+        //Interface
+        public ActionResult Index() {
+            // GET: jQuery
+            return View();
+        }
 
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Shipment([Bind(Include = "ShipDate")] Shipment shipment) {
-            // To protect from overposting attacks, please enable the specific properties you want to bind to, for more details see http://go.microsoft.com/fwlink/?LinkId=317598.
-           if (ModelState.IsValid) {
-                //
-                ViewData["message"] = "You submitted a ship date of " + shipment.ShipDate.ToString("MM/dd/yyyy");
-                return View(shipment);
-           }
-            return View(shipment);
+        public ActionResult FocusInputOnLoad() {
+            //Focus an  input control on apge load
+            return View();
+        }
+
+        public ActionResult ChangeFormElementState() {
+            //Change state of forms elements- copy fields, enable/disable fields
+            return View();
+        }
+
+        public ActionResult AutoSelectRadioButtons() {
+            //Automatically select a radio button
+            return View();
+        }
+
+        public ActionResult ToggleAllCheckboxes() {
+            //Provide a toggle to select/deselect all checkboxes
+            return View();
         }
     }
 }
---
+++ VS13.jQuery.Mvc/Controllers/jQueryUIController.cs
@@ -0,0 +1,29 @@
+﻿using System.Web.Mvc;
+using VS13.Models;
+
+namespace VS13.Controllers {
+    public class jQueryUIController:Controller {
+        // GET: jQuery
+        public ActionResult Index() { return View(); }
+        public ActionResult Calendar() { return View(new FromToDates()); }        
+        public ActionResult Spinner() { return View(); }
+        public ActionResult TabsH() { return View(); }
+        public ActionResult TabsV() { return View(); }
+        public ActionResult TimeSpinner() { return View(); }
+
+
+        public ActionResult Shipment() { return View(new Shipment()); }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Shipment([Bind(Include = "ShipDate")] Shipment shipment) {
+            // To protect from overposting attacks, please enable the specific properties you want to bind to, for more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+           if (ModelState.IsValid) {
+                //
+                ViewData["message"] = "You submitted a ship date of " + shipment.ShipDate.ToString("MM/dd/yyyy");
+                return View(shipment);
+           }
+            return View(shipment);
+        }
+    }
+}
