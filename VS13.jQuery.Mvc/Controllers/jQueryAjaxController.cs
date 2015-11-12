825444ff0199fa28c4b62fe3673e9abb14f5b713
jpheary <james.heary@hotmail.com>
2015-11-11 20:59:22 -0500

    Adding Ajax mechanisms.

---
+++ VS13.jQuery.Mvc/Controllers/jQueryAjaxController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace VS13.Controllers {
+    public class jQueryAjaxController:Controller {
+        //Members
+
+
+        //Interface
+        public ActionResult Index() {
+            // GET: jQueryAjax
+            return View();
+        }
+
+        public ActionResult StringReturn() {
+            // GET: jQueryAjax
+            return View();
+        }
+
+        [HttpGet]
+        public string StringCheck(string number) {
+            //Return a string- maybe validate something like odd/even and return an error message
+            if (int.Parse(number) % 2 == 1)
+                return "Odd numbers are not permited.";
+            else
+                return "";
+        }
+    }
+}
